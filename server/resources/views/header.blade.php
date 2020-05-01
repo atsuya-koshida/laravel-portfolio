31a2a23051370cc9281ef657367af3e6132efc6f
atsuya-koshida <koshi0845@gmail.com>
2020-05-02 07:58:58 +0900

    ヘッダーを切り出し

---
+++ server/resources/views/header.blade.php
@@ -0,0 +1,14 @@
+<header class="header">
+  <div class="header__inner">
+    <div class="header__left-box">
+      <p class="main-title">
+        Basket Ball
+      </p>
+    </div>
+    <ul class="header__right-box">
+      <li><a href="#">マイページ</a></li>
+      <li><a href="#">プロフィール編集</a></li>
+      <li><a href="#">ログアウト</a></li>
+    </ul>
+  </div>
+</header>
--- server/resources/views/home.blade.php
+++ server/resources/views/home.blade.php
@@ -4,38 +4,30 @@
 
 @section('content')
   {{-- ヘッダー --}}
-  <header class="header">
-    <div class="header__left-box">
-      <p class="header__title">
-        Basket Ball
-      </p>
-    </div>
-    {{-- dropdown --}}
-    <ul class="header__right-box">
-      <li><a href="#" class="link">マイページ</a></li>
-      <li><a href="#" class="link">プロフィール編集</a></li>
-      <li><a href="#" class="link">ログアウト</a></li>
-    </ul>
-  </header>
+  @include('header')
 
   <main class="main-wrapper">
-    {{-- トップページの画像・新着投稿 --}}
-    <div class="img-section"></div>
-    <div class="post-section">
-      <p class="post-section__title">新着投稿</p>
-      <div class="post-content"></div>
-    </div>
-
-    {{-- サイドバー --}}
-    <aside class="side-bar">
-      <div class="btn-section">
-        <ul class="btn-group">
-          <li class="big-btn"><a href="#" class="link">メンバーを募集する</a></li>
-          <li class="big-btn"><a href="#" class="link">チームを探す</a></li>
-          <li class="big-btn"><a href="#" class="link">チャット</a></li>
-        </ul>
+    <div class="main-wrapper__inner">
+      {{-- トップページの画像・新着投稿 --}}
+      <div class="main-container">
+        <div class="img-section"></div>
+        <div class="post-section">
+          <p class="post-section__title">新着投稿</p>
+          <div class="post-content"></div>
+        </div>
       </div>
-    </aside>
+  
+      {{-- サイドバー --}}
+      <aside class="side-bar">
+        <div class="btn-section">
+          <ul class="btn-group">
+            <li class="big-btn"><a href="#" class="link">メンバーを募集する</a></li>
+            <li class="big-btn"><a href="#" class="link">チームを探す</a></li>
+            <li class="big-btn"><a href="#" class="link">チャット</a></li>
+          </ul>
+        </div>
+      </aside>
+    </div>
   </main>
 
     
