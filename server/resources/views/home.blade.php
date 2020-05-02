2b03232ed82a2158263c3fc47aa063e7eee1e23e
atsuya-koshida <koshi0845@gmail.com>
2020-05-02 11:17:27 +0900

    sidebarを作成

--- server/resources/views/home.blade.php
+++ server/resources/views/home.blade.php
@@ -10,23 +10,19 @@
     <div class="main-wrapper__inner">
       {{-- トップページの画像・新着投稿 --}}
       <div class="main-container">
-        <div class="img-section"></div>
+        <div class="img-section">
+          <img src="/images/basket.jpeg" alt="" class="main-image">
+        </div>
         <div class="post-section">
-          <p class="post-section__title">新着投稿</p>
+          <div class="post-section__title">
+            <p>新着投稿</p>
+          </div>
           <div class="post-content"></div>
         </div>
       </div>
   
       {{-- サイドバー --}}
-      <aside class="side-bar">
-        <div class="btn-section">
-          <ul class="btn-group">
-            <li class="big-btn"><a href="#" class="link">メンバーを募集する</a></li>
-            <li class="big-btn"><a href="#" class="link">チームを探す</a></li>
-            <li class="big-btn"><a href="#" class="link">チャット</a></li>
-          </ul>
-        </div>
-      </aside>
+      @include('sidebar')
     </div>
   </main>
 
