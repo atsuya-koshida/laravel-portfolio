01ae21bdf4cf7cb2a03a60bb419288ee21691f7b
atsuya-koshida <koshi0845@gmail.com>
2020-05-02 14:29:30 +0900

    記事のビューを切り出し

--- server/resources/views/home.blade.php
+++ server/resources/views/home.blade.php
@@ -19,50 +19,7 @@
             <p>新着投稿</p>
           </div>
           {{-- 記事のカード --}}
-          <div class="blog-card">
-            <div class="meta">
-              <img src="/images/basket6.jpeg" class="photo">
-              <ul class="details">
-                <li class="author"><a href="#">ユーザー名</a></li>
-                <li class="date">2020/5/2</li>
-                <li class="tags">
-                  <ul>
-                    <li><a href="#">タグ</a></li>
-                    <li><a href="#">タグ</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            <div class="description">
-              <h1>タイトル</h1>
-              <p>本文が入る</p>
-              <p class="read-more">
-                <a href="#">詳しく見る</a>
-              </p>
-            </div>
-          </div>
-          <div class="blog-card">
-            <div class="meta">
-              <img src="/images/basket6.jpeg" class="photo">
-              <ul class="details">
-                <li class="author"><a href="#">ユーザー名</a></li>
-                <li class="date">2020/5/2</li>
-                <li class="tags">
-                  <ul>
-                    <li><a href="#">タグ</a></li>
-                    <li><a href="#">タグ</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            <div class="description">
-              <h1>タイトル</h1>
-              <p>本文が入る</p>
-              <p class="read-more">
-                <a href="#">詳しく見る</a>
-              </p>
-            </div>
-          </div>
+          @include('card')
         </div>
       </div>
   
