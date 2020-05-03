9f07884b7557c8dd92e14b6a0c53eb8f3fea45fb
atsuya-koshida <koshi0845@gmail.com>
2020-05-03 13:22:59 +0900

    検索用のフォームを作成

--- server/resources/views/posts/index.blade.php
+++ server/resources/views/posts/index.blade.php
@@ -12,7 +12,28 @@
         {{-- 絞り込み --}}
         <div class="narrow-down">
           <p>絞り込み</p>
-          <form action=""></form>
+          <form method="GET" action="">
+            <div class="search">
+              <input type="text" placeholder="検索">
+              <i class="fas fa-search"></i>
+            </div>
+            <div class="prefecture select-box">
+              <select>
+                <option value="" hidden>都道府県を選んでください</option>
+                <option value="1">北海道</option>
+                <option value="2">東京</option>
+                <option value="3">名古屋</option>
+                <option value="4">大阪</option>
+              </select>
+            </div>
+            <div class="sort">
+              <ul>
+                <li>並び順：</li>
+                <li><a href="#">新着順</a></li>
+                <li><a href="#">古い順</a></li>
+              </ul>
+            </div>
+          </form>
         </div>
         {{-- 投稿一覧 --}}
         <div class="post-section">
