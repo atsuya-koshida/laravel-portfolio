67489c18addf528df9fa4f4aad9d2be81408a504
atsuya-koshida <koshi0845@gmail.com>
2020-05-05 17:16:30 +0900

    post tableを作成

--- server/database/migrations/2020_05_05_054324_create_posts_table.php
+++ server/database/migrations/2020_05_05_054324_create_posts_table.php
@@ -14,7 +14,14 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title');
+            $table->string('team_name');
+            $table->string('activity_place');
+            $table->string('activity_time');
+            $table->text('description');
+            $table->integer('region_id')->unsigned();
+            $table->foreign('region_id')->references('id')->on('regions');
             $table->timestamps();
         });
     }
