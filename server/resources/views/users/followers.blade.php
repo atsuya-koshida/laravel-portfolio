46f0689afddff24009699aaec9cf750e539ad976
atsuya-koshida <koshi0845@gmail.com>
2020-05-11 21:00:45 +0900

    フォロワー一覧画面を作成

--- server/resources/views/users/followers.blade.php
+++ server/resources/views/users/followers.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 
-@section('title', $user->name . 'のフォロー')
+@section('title', $user->name . 'のフォロワー')
 
 @section('content')
   {{-- ヘッダー --}}
@@ -10,9 +10,9 @@
     <div class="main-wrapper__inner">
       <div class="main-container">
         {{-- ユーザー情報 --}}
-        <p class="user-section__title">{{ $user->name }}のフォロー</p>
+        <p class="user-section__title">{{ $user->name }}のフォロワー</p>
         <div class="user-section">
-          @foreach ($followings as $user)
+          @foreach ($followers as $user)
           <div class="user-section__top" style="margin-bottom: 15px;">
             <div class="user-section__top--left">
               <div class="user-image">
