bcc1bec33489d7dc37f353a290d0f491aad56516
atsuya-koshida <koshi0845@gmail.com>
2020-05-12 13:56:35 +0900

    user, groupのリレーションを定義

--- server/app/Group.php
+++ server/app/Group.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Group extends Model
 {
-    //
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
