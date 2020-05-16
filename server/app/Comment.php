18feb35e527d06b7cd352b038fd78e67dfc7120d
atsuya-koshida <koshi0845@gmail.com>
2020-05-16 22:56:10 +0900

    リレーションを定義

--- server/app/Comment.php
+++ server/app/Comment.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'text',
+        'post_id',
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo('App\Post');
+    }
 }
